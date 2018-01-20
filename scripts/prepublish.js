ac0b2166d131aa2881f32f7e5f63aad87dd4c76c
Mateo Tibaquirá <nestormateo@gmail.com>
2018-01-20 18:52:16 -0500

    fix: prepublish script synced
    adapter package.json updated

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -5,11 +5,24 @@ const fs = require('fs');
 cpx.copy('README.md', 'dist');
 cpx.copy('CHANGELOG.md', 'dist');
 
-const packageJson = JSON.parse(fs.readFileSync('dist/package.json'));
+// datepicker
+let packageJson = JSON.parse(fs.readFileSync('dist/package.json'));
+
 delete packageJson['$schema'];
 delete packageJson['devDependencies'];
 delete packageJson['scripts'];
 delete packageJson['private'];
 delete packageJson['ngPackage'];
 delete packageJson['files'];
+
 fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, undefined, 2));
+
+// moment-adapter
+packageJson = JSON.parse(fs.readFileSync('dist/moment-adapter/package.json'));
+
+delete packageJson['$schema'];
+
+fs.writeFileSync(
+  'dist/moment-adapter/package.json',
+  JSON.stringify(packageJson, undefined, 2)
+);
